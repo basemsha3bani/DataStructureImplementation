eab0fe0b9b7612d10960b5cabbbcb7f8efe603b1
basem <basem_sha3bani@hotmaill.com>
2026-09-11 07:21:01 +0300

    Add graph data structure implementation and tests
    A new `Graph` project has been added to the solution, targeting .NET 8.0. It includes the `GraphImplementation` class for representing and traversing graphs using nodes (`GraphNode`). The `Traverse` method performs depth-first traversal.
    
    A corresponding `GraphTest` class has been added to validate the functionality of the graph implementation. The solution and test project files have been updated to include the new `Graph` project and its build configurations.

---
+++ Graph/GraphImplementation.cs
@@ -0,0 +1,61 @@
+﻿using Common;
+
+namespace Graph
+{
+    public class GraphImplementation
+    {
+
+       public List<GraphNode> graphNodes { get; set; } = new List<GraphNode>();
+
+
+        public GraphImplementation() { }
+
+        public void AddEdge(GraphNode node)
+        {
+            graphNodes.Add(node);
+        }
+        public List<int> Traverse(GraphNode start)
+        {
+            List<int> visitedNodes= new List<int>();
+            visitedNodes.Add(start.element.value);
+            if (start.connectedElements == null)
+            {
+                return visitedNodes;
+            }
+            foreach (GraphNode node in start.connectedElements)
+            {
+                visitedNodes.Add(node.element.value);
+                {
+                    GraphNode childNode;
+                    if (node.connectedElements == null) continue;
+                    foreach (GraphNode connectedNode in node.connectedElements)
+                    {
+                        childNode = connectedNode;
+                        if(!visitedNodes.Contains(connectedNode.element.value))
+                        {
+                            visitedNodes.Add(connectedNode.element.value);
+                            visitedNodes = visitedNodes.Union(Traverse(connectedNode)).ToList();
+                            //instead of this line add
+                            //visitedNodes = visitedNodes.Union(TraverseChildren(node.connectedElements)).ToList();
+                        }
+
+
+
+
+
+
+                    }
+
+
+                }
+
+            }
+            return visitedNodes;
+        }
+    }
+        public class GraphNode
+        {
+            public Element element { get; set; }
+            public List<GraphNode> connectedElements { get;  set; }
+        }
+}
---
+++ TestProject2/GraphTest.cs
@@ -0,0 +1,63 @@
+﻿using Graph;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    [TestClass]
+    public class GraphTest
+    {
+        [TestMethod]
+        public void TestGraph()
+        {
+            GraphImplementation graphImplementation;
+            graphImplementation = new GraphImplementation();
+            graphImplementation.AddEdge(new GraphNode
+            {
+                element = new Common.Element(1, null),
+                connectedElements = new List<GraphNode>()
+                {
+                    new GraphNode
+                    {
+                        element= new Common.Element(2,null),
+                        connectedElements = new List<GraphNode>()
+                             {
+                                        new GraphNode
+                                        {
+                                            element= new Common.Element(4,null)
+
+                                        },
+                             }
+                    },
+                    new GraphNode
+                    {
+                        element= new Common.Element(3,null),
+                         connectedElements = new List<GraphNode>()
+                             {
+                                    new GraphNode
+                                    {
+                                        element= new Common.Element(5,null)
+
+                                    }
+                             }
+                    }
+                }
+                    
+
+
+
+                
+            });
+          
+
+           ;
+
+            List<int> x = graphImplementation.Traverse(graphImplementation.graphNodes[0]);
+
+        }
+    }
+}
